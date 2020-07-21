bbb906e9859c87ab07c81d6ede12e69ab60c14df
abbasShDev <abbas20alzaeem@gmail.com>
2020-07-21 05:06:08 +0300

    header  done

--- js/app.js
+++ js/app.js
@@ -20,4 +20,78 @@ $(document).ready(function() {
 			);
 		}
 	);
+
+	//header text typing
+	$('.header-text').each(function() {
+		let headerH1 = $(this).find('h1'),
+			h1Text = headerH1.data('text'),
+			h1Text2 = headerH1.data('text2'),
+			h1TextLength = h1Text.length,
+			h1TextLength2 = h1Text2.length;
+
+		let headerPara = $(this).find('p'),
+			paraText = headerPara.data('text'),
+			paraText2 = headerPara.data('text2'),
+			paraTextLength = paraText.length,
+			paraTextLength2 = paraText2.length;
+
+		//start typing
+		startTyping();
+		//start typing with the interval
+		setInterval(function() {
+			startTyping();
+		}, 8000);
+
+		function startTyping() {
+			headerH1
+				.queue(function() {
+					typingText(headerH1, h1Text, h1TextLength);
+					typingText(headerPara, paraText, paraTextLength);
+					$(this).dequeue();
+				})
+				.delay(3000)
+				.queue(function() {
+					headerH1.html('');
+					headerPara.html('');
+					$(this).dequeue();
+				})
+				.queue(function() {
+					typingText(headerH1, h1Text2, h1TextLength2);
+					typingText(headerPara, paraText2, paraTextLength2);
+					$(this).dequeue();
+				});
+		}
+	});
+
+	//initalizing type by intarvel
+	function typingText(selector, text, textLength) {
+		selector.html('');
+		let n = 0,
+			intervalName = setInterval(function() {
+				selector.each(function() {
+					$(this).find('.cursor').remove();
+					$(this).html($(this).html() + text[n]);
+					let cursor = '<span class="cursor">|</span>';
+					$(this).append(cursor);
+				});
+
+				n++;
+
+				if (n === textLength) {
+					selector.each(function() {
+						$(this).find('.cursor').remove();
+					});
+					clearInterval(intervalName);
+				}
+			}, 70);
+	}
+
+	//random background
+
+	setInterval(function() {
+		//randomNumber from 0-9
+		let randomNum = Math.floor(Math.random() * 10);
+
+		$('.header').css('backgroundImage', `url(css/imgs/header-${randomNum}.jpg)`);
+	}, 10000);
 });
