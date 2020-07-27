a19b511b5d61202de00ad81f0c46e765c0d1ac59
abbasShDev <abbas20alzaeem@gmail.com>
2020-07-27 14:53:08 +0300

    loading gif added

--- js/app.js
+++ js/app.js
@@ -367,3 +367,7 @@ $(document).ready(function() {
 $('.navbar-toggler').click(function() {
 	$('.navbar-toggler').toggleClass('active-navbar');
 });
+
+$(window).on('load', function() {
+	$('.loading-screen').fadeOut();
+});
